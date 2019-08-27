27b3652e17e811056d063853385e32d23efa678b
nnk <nnkumar13@gmail.com>
2019-08-27 16:20:36 -0700

    Final working changes

---
+++ src/main/scala/com/sparkbyexamples/Employee.scala
@@ -0,0 +1,7 @@
+package com.sparkbyexamples
+
+
+case class Employee(key: String, fName: String, lName: String,
+                    mName:String, addressLine:String, city:String,
+                    state:String, zipCode:String)
+
--- src/main/scala/com/sparkbyexamples/HBaseSparkExample.scala
+++ src/main/scala/com/sparkbyexamples/HBaseSparkExample.scala
@@ -14,28 +14,59 @@ object HBaseSparkExample {
          |"table":{"namespace":"default", "name":"employee"},
          |"rowkey":"key",
          |"columns":{
-         |"key1":{"cf":"rowkey", "col":"key", "type":"string"},
-         |"name1":{"cf":"person", "col":"name", "type":"string"},
-         |"state1":{"cf":"address", "col":"state", "type":"string"}
+         |"key":{"cf":"rowkey", "col":"key", "type":"string"},
+         |"fName":{"cf":"person", "col":"firstName", "type":"string"},
+         |"lName":{"cf":"person", "col":"lastName", "type":"string"},
+         |"mName":{"cf":"person", "col":"middleName", "type":"string"},
+         |"addressLine":{"cf":"address", "col":"addressLine", "type":"string"},
+         |"city":{"cf":"address", "col":"city", "type":"string"},
+         |"state":{"cf":"address", "col":"state", "type":"string"},
+         |"zipCode":{"cf":"address", "col":"zipCode", "type":"string"}
          |}
          |}""".stripMargin
 
-    println(catalog)
 
-    val data = (0 to 10).map { i => HBaseRecord(i+"", i+"Naveen", i+"Santa Ana") }
+    val data = Seq(Employee("1", "Abby", "Smith", "K", "3456 main", "Orlando", "FL", "45235"),
+      Employee("2", "Amaya", "Williams", "L", "123 Orange", "Newark", "NJ", "27656"),
+      Employee("3", "Alchemy", "Davis", "P", "Warners", "Sanjose", "CA", "34789"))
 
-      val spark: SparkSession = SparkSession.builder()
-        .master("local[1]")
-        .appName("SparkByExamples.com")
-        .getOrCreate()
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("SparkByExamples.com")
+      .getOrCreate()
 
-      import spark.implicits._
-      val df = spark.sparkContext.parallelize(data).toDF
+    import spark.implicits._
+    val df = spark.sparkContext.parallelize(data).toDF
 
-        df.write.options(
-          Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5"))
-       // .format("org.apache.hadoop.hbase.spark")
-          .format("org.apache.spark.sql.execution.datasources.hbase") // https://github.com/hortonworks-spark/shc
-        .save()
+    df.write.options(
+      Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "3"))
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .save()
+
+
+    // Reading from HBase to DataFrame
+    val hbaseDF = spark.read
+      .options(Map(HBaseTableCatalog.tableCatalog -> catalog))
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .load()
+
+    //Display Schema from DataFrame
+    hbaseDF.printSchema()
+
+    //Collect and show Data from DataFrame
+    hbaseDF.show(false)
+
+    //Applying Filters
+    hbaseDF.filter($"key" === "1" && $"state" === "FL")
+      .select("key", "fName", "lName")
+      .show()
+
+    //Create Temporary Table on DataFrame
+    hbaseDF.createOrReplaceTempView("employeeTable")
+
+    //Run SQL
+    spark.sql("select * from employeeTable where fName = 'Amaya' ").show
+
+
   }
 }
